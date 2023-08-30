bcc88af606ca435a4a907ae1ca8007e691aedaab
Arighna Chakraborty <riju23chakra@gmail.com>
2023-08-30 10:43:00 +0530

    initial commit

--- lib/views/screens/screens.dart
+++
@@ -1,4 +0,0 @@
-export 'home_screen.dart';
-export 'search_screen.dart';
-export 'settings_screen.dart';
-export 'splash_screen.dart';
--- lib/views/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export "nav_bar_widget.dart";
-export "word_card_widget.dart";
-export "category_widget.dart";
