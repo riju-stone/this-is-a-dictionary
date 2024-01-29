944dba77782575c437f6ecbe6841b457eae9fb4a
Arighna Chakraborty <riju23chakra@gmail.com>
2024-01-29 16:58:12 +0530

    updated: expo sdk 50

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-
-import NavigationBar from "./src/navigation/NavigationBar";
 
 export default function App() {
-	return (
-		<SafeAreaProvider>
-			<StatusBar style="auto" />
-			<NavigationBar />
-		</SafeAreaProvider>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- README.md
+++
@@ -1,2 +0,0 @@
-# this-is-a-dictionary
-A Powerful English Dictionary
