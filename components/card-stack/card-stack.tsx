8e69d35b2ff9f60c70f489d3b273cae7bff43b58
Arighna Chakraborty <riju23chakra@gmail.com>
2024-10-06 01:57:50 +0530

    implemented card interpolation logic

--- components/card-stack/card-stack.tsx
+++ components/card-stack/card-stack.tsx
@@ -1,14 +1,55 @@
 import { View, StyleSheet } from "react-native";
 import React from "react";
+import { useSharedValue } from "react-native-reanimated";
 import CardComponent from "../card/card";
 
+const cardData = [
+  {
+    word: "accept",
+    pronounciation: "Akcept",
+    meaning: "To consider something",
+  },
+  {
+    word: "accept",
+    pronounciation: "Akcept",
+    meaning: "To consider something",
+  },
+  {
+    word: "accept",
+    pronounciation: "Akcept",
+    meaning: "To consider something",
+  },
+  {
+    word: "accept",
+    pronounciation: "Akcept",
+    meaning: "To consider something",
+  },
+];
+
 const CardStackComponent = () => {
+  const animatedValue = useSharedValue(0);
+  const currentIndex = useSharedValue(0);
+  const previousIndex = useSharedValue(0);
+
   return (
     <View style={styles.cardStackContainer}>
-      <CardComponent stackPos={0} />
-      <CardComponent stackPos={1} />
-      <CardComponent stackPos={2} />
-      <CardComponent stackPos={3} />
+      {cardData.map((data, index) => {
+        return (
+          <CardComponent
+            key={`card-${index}`}
+            stackPos={index}
+            currentIndex={currentIndex}
+            previousIndex={previousIndex}
+            animatedValue={animatedValue}
+            cardData={data}
+            dataLength={cardData.length}
+          />
+        );
+      })}
+      {/* <CardComponent stackPos={0} /> */}
+      {/* <CardComponent stackPos={1} /> */}
+      {/* <CardComponent stackPos={2} /> */}
+      {/* <CardComponent stackPos={3} /> */}
     </View>
   );
 };
--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,19 +1,95 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
+import { SharedValue } from "react-native-reanimated";
+import Animated, {
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+} from "react-native-reanimated";
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from "react-native-gesture-handler";
 
-const CardComponent = ({ stackPos }: { stackPos: number }) => {
+type CardData = {
+  word: string;
+  pronounciation: string;
+  meaning: string;
+};
+
+type CardProps = {
+  currentIndex: SharedValue<number>;
+  previousIndex: SharedValue<number>;
+  animatedValue: SharedValue<number>;
+  stackPos: number;
+  cardData: CardData;
+  dataLength: number;
+};
+
+const CardComponent = (props: CardProps) => {
+  const animatedStyle = useAnimatedStyle(() => {
+    const translateY = interpolate(
+      props.animatedValue.value,
+      [props.stackPos - 1, props.stackPos, props.stackPos + 1],
+      [-42, 1, 42],
+    );
+    const scale = interpolate(
+      props.animatedValue.value,
+      [props.stackPos - 1, props.stackPos, props.stackPos + 1],
+      [0.92, 1, 1.1],
+    );
+    const opacity = interpolate(
+      props.animatedValue.value,
+      [props.stackPos - 1, props.stackPos, props.stackPos + 1],
+      [1, 1, 0],
+    );
+    return {
+      transform: [{ translateY }, { scale }],
+    };
+  });
+
   return (
-    <View
-      style={{
-        zIndex: stackPos,
-        width: `${85 + stackPos * 2}%`,
-        bottom: `-${stackPos * 2}%`,
-        ...styles.wordCardContainer,
+    <FlingGestureHandler
+      key={"right"}
+      direction={Directions.RIGHT}
+      onHandlerStateChange={(event) => {
+        if (event.nativeEvent.state === State.END) {
+          props.animatedValue.value = withTiming(
+            (props.currentIndex.value += 1),
+          );
+
+          props.previousIndex.value = props.currentIndex.value - 1;
+        }
       }}
     >
-      <Text>Word Details</Text>
-    </View>
+      <FlingGestureHandler
+        key={"left"}
+        direction={Directions.LEFT}
+        onHandlerStateChange={(event) => {
+          if (event.nativeEvent.state === State.END) {
+            props.animatedValue.value = withTiming(
+              (props.currentIndex.value += 1),
+            );
+
+            props.previousIndex.value = props.currentIndex.value - 1;
+          }
+        }}
+      >
+        <Animated.View
+          style={[
+            {
+              zIndex: props.dataLength - props.stackPos,
+              ...styles.wordCardContainer,
+            },
+            animatedStyle,
+          ]}
+        >
+          <Text>Word Details</Text>
+        </Animated.View>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
   );
 };
 
@@ -21,6 +97,7 @@ const styles = StyleSheet.create({
   wordCardContainer: {
     position: "absolute",
     height: "80%",
+    width: "90%",
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     backgroundColor: Colors.primaryColor,
